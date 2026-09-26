636b17449087e994c0603a3e47fcb5141b42593d
Localiiiy <pradeepchandrazzwarr@gmail.com>
2026-09-26 08:40:50 +0530

    build: disable minification and shrink for release
    Revert obfuscation and resource shrinking for the release build to simplify debugging and deployment configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,9 @@ android {
   buildTypes {
     release {
       isCrunchPngs = true
-      isMinifyEnabled = true
-      isShrinkResources = true
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
+      isMinifyEnabled = false
+      isShrinkResources = false
+      signingConfig = signingConfigs.getByName("debugConfig")
     }
     debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
